2771e4a10e44c7804da8006f2afb960c4b4d0c59
Júlia Cristina Lopes Coimbra <114704249+JuliaLopesCoimbra@users.noreply.github.com>
2023-04-10 17:59:49 -0300

    adicionei mais exercicios e modificações do site

--- ex.js
+++ ex.js
@@ -1,3 +1,4 @@
+//lista01
 function calculaLosango(){
     let dM = Number(document.getElementById('dM').value);
     let dm = Number(document.getElementById('dm').value);
@@ -38,5 +39,155 @@ function calculaTabuada(){
 function calculaIdade(){
     let idade = new Date(document.getElementById('DataNasci').value);
     let calc = Math.floor((Date.now() - idade) / (31557600000));
-    Number(document.getElementById('ex14').innerHTML = "tem idade em anos = " + calc + " anos")
+    Date(document.getElementById('calc14').innerHTML = "idade em anos = " + calc + " anos")
+    Date(document.getElementById('calc14a').innerHTML = "idade em meses = " + calc + " anos")
+    Date(document.getElementById('calc14b').innerHTML = "idade em dias = " + calc + " anos")
+    Date(document.getElementById('calc14c').innerHTML = "idade em semanas = " + calc + " anos")
+
+
+}
+
+
+//lista02
+function calculaNota(){
+    let n1 = Number(document.getElementById('nota1').value);
+    let n2 = Number(document.getElementById('nota2').value);
+    let n3 = Number(document.getElementById('nota3').value);
+    let n4 = Number(document.getElementById('nota4').value);
+    let calc = (n1+n2+n3+n4)/4
+    if(calc < 7 && calc > 0){
+        Number(document.getElementById('calc01').innerHTML = "reprovado" )
+    }else if (calc >= 7){
+        Number(document.getElementById('calc01').innerHTML = "aprovado" )   
+    }
+}
+function calculaTabNota(){
+    let n1 = Number(document.getElementById('nota1tab').value);
+    let n2 = Number(document.getElementById('nota2tab').value);
+    let calc = (n1+n2)/2
+    Number(document.getElementById('calc02').innerHTML = "média aritmética da nota = " + calc )
+}
+function calculamairoumenor(){
+    let n1 = Number(document.getElementById('n1M').value);
+    let n2 = Number(document.getElementById('n2M').value);
+    if(n1<n2){
+        Number(document.getElementById('calc03').innerHTML = "maior = " + n2 + "  menor = "+n1 )
+    }else{
+        Number(document.getElementById('calc03').innerHTML = "maior = " + n1 + "  menor = "+n2 )
+    }
+    if(n1==n2){
+        Number(document.getElementById('calc03').innerHTML = "iguais = " + n1 )
+    }
+}
+function calculamairoumenor2(){
+    let n1 = Number(document.getElementById('n1').value);
+    let n2 = Number(document.getElementById('n2').value);
+    let n3 = Number(document.getElementById('n3').value);
+    if(n1 > n2 && n2 > n3){
+        Number(document.getElementById('calc04').innerHTML = "o maior número é " +n1+" depois " +n2+" e por ultimo " +n3 )
+    }else if (n1>n3 && n3>n2){
+        Number(document.getElementById('calc04').innerHTML = "o maior número é " +n1+" depois " +n3+" e por ultimo " +n2 )
+    }else if (n2>n3 && n3>n1){
+        Number(document.getElementById('calc04').innerHTML = "o maior número é " +n2+" depois " +n3+" e por ultimo " +n1 )
+    }else if (n2>n1 && n1>n3){
+        Number(document.getElementById('calc04').innerHTML = "o maior número é " +n2+" depois " +n1+" e por ultimo " +n3 )
+    }else if (n3>n2 && n2>n1){
+        Number(document.getElementById('calc04').innerHTML = "o maior número é " +n3+" depois " +n2+" e por ultimo " +n1 )
+    }else if (n3>n1 && n1>n2){
+        Number(document.getElementById('calc04').innerHTML = "o maior número é " +n3+" depois " +n1+" e por ultimo " +n2 )
+    }else if(n1==n2==n3){
+        Number(document.getElementById('calc04').innerHTML = "todos os números são iguais" )
+    }
+}
+function calculaEscolha(){
+     // recuperar valores nro1 e nro2 do usuário
+     let n1 = Number(document.getElementById("n1E").value);
+     let n2 = Number(document.getElementById("n2E").value);
+     // recupera o valor da opção escolhida
+     let opcao = Number(document.getElementById("n3E").value);
+     // operar nos números de acordo com escolha do usuário
+     let resultado
+     switch(opcao) {
+         case 1: resultado = (n1 + n2) / 2
+                 break // não entra nos cases abaixo
+         case 2: if (n1 >= n2){
+                     resultado = n1 - n2 // nro1 é maior
+                 }
+                 else {
+                     resultado = n2 - n1 // nro2 é maior
+                 }
+                 break
+         case 3: resultado = n1 * n2
+                 break
+         case 4: if (n2 != 0){
+                     resultado = n1 / n2
+                 }
+                 else {
+                     resultado = "Divisão por zero"
+                 }
+                 break
+         default: resultado = "Opção inválida"
+     }
+     // saída dos dados
+     document.getElementById("calc05").innerHTML = resultado
+}
+function calculaEscolha2(){
+    let n1 = Number(document.getElementById("n1E2").value);
+    let n2 = Number(document.getElementById("n2E2").value);
+    let opcao = Number(document.getElementById("n3E2").value);
+    let resultado
+    let resultado2
+    switch(opcao){
+        case 1: resultado = Math.pow(n1,n2)
+            break
+        case 2:
+            resultado = Math.sqrt(n1)
+            resultado2 = Math.sqrt(n2)
+            break
+        case 3:
+            resultado = Math.cbrt(n1)
+            resultado2 = Math.cbrt(n2)
+        default: resultado = "Opção inválida"
+    }
+    document.getElementById("calc06").innerHTML = resultado 
+    document.getElementById("calc06_2").innerHTML = resultado2
+}
+function calculaAumento(){
+    let sal = Number(document.getElementById("sal").value);
+    let calc
+    if(sal <= 500 && sal>0){
+        calc = sal+(sal*0.3)
+        document.getElementById("calc07").innerHTML = "Valor do Salário com aumento = "+calc 
+    }else{
+        calc = sal
+        document.getElementById("calc07").innerHTML = "Não há aumento | Valor do Salário = "+calc
+    }
+}
+function calculaAumento2(){
+    let sal = Number(document.getElementById("sal2").value);
+    let calc
+    if(sal <= 300 && sal>0){
+        calc = sal+(sal*0.35)
+        document.getElementById("calc08").innerHTML = "Valor do Salário com aumento = "+calc 
+    }else if(sal > 300){
+        calc = sal+(sal*0.15)
+        document.getElementById("calc08").innerHTML = "Valor do Salário com aumento = "+calc
+    }
+}
+function calculaSM(){
+    let sal = Number(document.getElementById("SM").value);
+    let calc
+    if(sal > 400){
+        calc = sal+(sal*0.3)
+        document.getElementById("calc09").innerHTML = "Valor do Salário com aumento de crétidos de 30% = "+calc 
+    }else if(sal <= 400 && sal>300){
+        calc = sal+(sal*0.25)
+        document.getElementById("calc09").innerHTML = "Valor do Salário com aumento de crétidos de 25% = "+calc 
+    }else if(sal <= 300 && sal>200){
+        calc = sal+(sal*0.2)
+        document.getElementById("calc09").innerHTML = "Valor do Salário com aumento de crétidos de 20% = "+calc 
+    }else if(sal <= 200 && sal>0){
+        calc = sal+(sal*0.1)
+        document.getElementById("calc09").innerHTML = "Valor do Salário com aumento de crétidos de 10% = "+calc 
+    }
 }
